b6e3f6cfe791816e3aa08689121176208621b723
Esmaeil Saleh <36760412+EsmaeilSaleh@users.noreply.github.com>
2026-02-02 07:51:44 +0100

    main.c 25 lines

--- src/main.c
+++ src/main.c
@@ -12,11 +12,45 @@
 
 #include "so_long.h"
 
+static void	load_map(t_game *game, char *path)
+{
+	char	*content;
+	int		height;
+
+	if (!is_ber_file(path))
+		error_exit("Map must have .ber extension", game);
+	content = read_file(path);
+	if (!content)
+		error_exit("Failed to read map", game);
+	game->map = split_map(content, &height);
+	(void)height;
+	free(content);
+	if (!game->map)
+		error_exit("Invalid map format", game);
+	validate_map(game);
+	check_path(game);
+	game->map[game->player_y][game->player_x] = '0';
+}
+
+static void	start_game(t_game *game)
+{
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		error_exit("Failed to init MLX", game);
+	game->win = mlx_new_window(game->mlx, game->width * TILE_SIZE,
+			game->height * TILE_SIZE, "so_long");
+	if (!game->win)
+		error_exit("Failed to create window", game);
+	load_textures(game);
+	render_map(game);
+	mlx_hook(game->win, 17, 0, handle_close, game);
+	mlx_hook(game->win, 2, 1L << 0, handle_key, game);
+	mlx_loop(game->mlx);
+}
+
 int	main(int argc, char **argv)
 {
 	t_game	game;
-	char	*content;
-	int		height;
 
 	init_game(&game);
 	if (argc != 2)
@@ -24,30 +58,7 @@ int	main(int argc, char **argv)
 		ft_putstr_fd("Error\nUsage: ./so_long <map.ber>\n", 2);
 		return (1);
 	}
-	if (!is_ber_file(argv[1]))
-		error_exit("Map must have .ber extension", &game);
-	content = read_file(argv[1]);
-	if (!content)
-		error_exit("Failed to read map", &game);
-	game.map = split_map(content, &height);
-	(void)height;
-	free(content);
-	if (!game.map)
-		error_exit("Invalid map format", &game);
-	validate_map(&game);
-	check_path(&game);
-	game.map[game.player_y][game.player_x] = '0';
-	game.mlx = mlx_init();
-	if (!game.mlx)
-		error_exit("Failed to init MLX", &game);
-	game.win = mlx_new_window(game.mlx, game.width * TILE_SIZE,
-			game.height * TILE_SIZE, "so_long");
-	if (!game.win)
-		error_exit("Failed to create window", &game);
-	load_textures(&game);
-	render_map(&game);
-	mlx_hook(game.win, 17, 0, handle_close, &game);
-	mlx_hook(game.win, 2, 1L << 0, handle_key, &game);
-	mlx_loop(game.mlx);
+	load_map(&game, argv[1]);
+	start_game(&game);
 	return (0);
 }
